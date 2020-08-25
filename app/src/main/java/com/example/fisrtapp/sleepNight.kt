a8104df78aeb910f891da7a2470d2b3abc0da253
a <pamika.si@rmuti.ac.th>
2020-08-25 18:19:33 +0700

    add data in sleepNight.kt

--- app/src/main/java/com/example/fisrtapp/sleepNight.kt
+++ app/src/main/java/com/example/fisrtapp/sleepNight.kt
@@ -1,4 +1,26 @@
 package com.example.fisrtapp
 
-class sleepNight {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+data class SleepNight(
+    var nightId: Long = 0L,
+    val startTimeMilli:Long = System.currentTimeMillis(),
+    var endtimeMilli: Long = startTimeMilli,
+    var sleepQuality: Int = -1
+)
+@Entity(tableName = "daily_sleep_quality_table")
+data class sleepNight(
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
+
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeMilli: Long = System.currentTimeMillis(),
+
+    @ColumnInfo(name = "end_time_milli")
+    var endTimeMilli: Long = startTimeMilli,
+
+    @ColumnInfo(name = "quality_rating")
+    var sleepQuality: Int = -1
+)
